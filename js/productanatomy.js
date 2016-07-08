55cc7c67cdb8bd00430af3dc4f9e0c29fdeb953e
mlejva <vasek.mlejnsky@gmail.com>
2016-07-08 20:24:21 +0200

    Implemented tags

--- js/productanatomy.js
+++ js/productanatomy.js
@@ -6,6 +6,9 @@ const BOOTSTRAP_MAX_WIDTH = 12;
 
 const SEARCHBOX_ID = 'searchbox';
 
+const DIV_CLASS_TAG_WRAPPER = 'tag-wrapper';
+const DIV_CLASS_TAG = 'tag';
+
 /* ----- Constant for static product card ----- */
 const DIV_CLASS_PRODUCT_DESCRIPTION = 'product-description';
 
@@ -30,6 +33,7 @@ const DIV_TEXT_PRODUCT_LAUNCHED_MODAL = 'Launched on ';
 
 const DIV_CLASS_PRODUCT_FOUNDERS_MODAL = 'product-founders-modal';
 const DIV_CLASS_PRODUCT_FOUNDERS_NAMES_WRAPPER_MODAL = 'founders-names-wrapper';
+const FOUNDER_TAG_COLOR = '#d0021b';
 const DIV_TEXT_PRODUCT_FOUNDERS_MODAL = 'Founded by ';
 
 const DIV_CLASS_PRODUCT_PLATFORMS_MODAL = 'product-platforms-modal';
@@ -69,6 +73,7 @@ const FIREBASE_PRODUCT_FOUNDERS = 'founders';
 const FIREBASE_PRODUCT_FOUNDERS_TWITTER = 'founders-twitter';
 const FIREBASE_PRODUCT_API = 'api';
 const FIREBASE_PRODUCT_TECHNOLOGY = 'technology';
+const FIREBASE_PRODUCT_LOGO_URL = 'logo-url'
 /* ---------- */
 
 /* Constants for Twitter */
@@ -126,6 +131,21 @@ function changeHeightOfCards() {
     $('div.card').css('height', '100%');
   }
 }
+// TODO: Make my own
+var tag_colors = ['orange', 'teal', 'cyan', 'goldenrod', 'purple', 'blue', 'pink', 'brown'];
+var randomColorFromString = function (str, colors) {
+    function digitize (str) {
+        var code = 0;
+        if (str === undefined) { return code; }
+        for (var i = 0; i < str.length; i++)
+            code += str.toLowerCase().charCodeAt(i);
+        console.log(code);
+        return code;
+    }
+
+    var code = digitize(str);
+    return colors[(code % colors.length)];
+};
 /* ---------- */
 
 
@@ -146,16 +166,20 @@ $(document).ready(function() {
       cardNumber++;
 
       /* ----- Card HTML ----- */
-      var cardHTML = '<div class=\"col-lg-4 bottom-buffer\">' +
-                      '<div data-toggle=\"modal\" data-target=\"#card' + cardNumber + '\" class=\"card\">' +
-                        '<img class=\"card-img-top\" src=\"' + product['logo-url'] + '\" alt=\"Product Logo\">' +
-                        '<div class=\"card-block\">' +
-                          '<div class=\"' + DIV_CLASS_PRODUCT_DESCRIPTION + '\">' +
-                            '<div class=\"row\">' +
-                              product['description'] +
-                            '</div>' +
-                          '</div>';
+      var cardHTML = '<div class=\"col-lg-4\">' +
+                      '<div data-toggle=\"modal\" data-target=\"#card' + cardNumber + '\" class=\"card\">';
 
+      if (FIREBASE_PRODUCT_LOGO_URL in product) {
+        cardHTML += '<img class=\"card-img-top\" src=\"' + product[FIREBASE_PRODUCT_LOGO_URL] + '\" alt=\"Product Logo\">';
+      }
+      cardHTML += '<div class=\"card-block\">';
+      if (FIREBASE_PRODUCT_DESCRIPTION in product) {
+        cardHTML += '<div class=\"' + DIV_CLASS_PRODUCT_DESCRIPTION + '\">' +
+                      '<div class=\"row\">' +
+                        product[FIREBASE_PRODUCT_DESCRIPTION] +
+                      '</div>' +
+                    '</div>';
+      }
       if (FIREBASE_PRODUCT_PLATFORMS in product) {
         var platforms = [];
         for (var platform_property in product[FIREBASE_PRODUCT_PLATFORMS]) {
@@ -216,67 +240,76 @@ $(document).ready(function() {
       }
 
       // Add description //
-      cardModalHTML += '<div class=\"row\">' +
-                        '<div class=\"' + DIV_CLASS_PRODUCT_DESCRIPTION_MODAL + '\">' +
-                          product[FIREBASE_PRODUCT_DESCRIPTION] +
-                        '</div>' +
-                      '</div>';
+      if (FIREBASE_PRODUCT_DESCRIPTION in product) {
+        cardModalHTML += '<div class=\"row\">' +
+                          '<div class=\"' + DIV_CLASS_PRODUCT_DESCRIPTION_MODAL + '\">' +
+                            product[FIREBASE_PRODUCT_DESCRIPTION] +
+                          '</div>' +
+                        '</div>';
+      }
 
       // Add launched //
-      cardModalHTML += '<div class=\"row\">' +
-                        '<div class=\"' + DIV_CLASS_PRODUCT_LAUNCHED_MODAL + '\">' +
-                          '<b>' + DIV_TEXT_PRODUCT_LAUNCHED_MODAL + '</b>' + product[FIREBASE_PRODUCT_LAUNCHED] +
-                        '</div>' +
-                      '</div>';
+      if (FIREBASE_PRODUCT_LAUNCHED in product) {
+        cardModalHTML += '<div class=\"row\">' +
+                          '<div class=\"' + DIV_CLASS_PRODUCT_LAUNCHED_MODAL + '\">' +
+                            '<b>' + DIV_TEXT_PRODUCT_LAUNCHED_MODAL + '</b>' + product[FIREBASE_PRODUCT_LAUNCHED] +
+                          '</div>' +
+                        '</div>';
+      }
 
       // Add founders //
-      // Get twitter usernames of founders
-      var founders_twitter_url = [];
-      if (FIREBASE_PRODUCT_FOUNDERS_TWITTER in product) {
-        for (var founder_twitter_property in product[FIREBASE_PRODUCT_FOUNDERS_TWITTER]) {
-          var url = TWITTER_BASE_ADDRESS + product[FIREBASE_PRODUCT_FOUNDERS_TWITTER][founder_twitter_property];
-          founders_twitter_url.push(url);
+      if (FIREBASE_PRODUCT_FOUNDERS in product) {
+        // Get twitter usernames of founders
+        var founders_twitter_url = [];
+        if (FIREBASE_PRODUCT_FOUNDERS_TWITTER in product) {
+          for (var founder_twitter_property in product[FIREBASE_PRODUCT_FOUNDERS_TWITTER]) {
+            var url = TWITTER_BASE_ADDRESS + product[FIREBASE_PRODUCT_FOUNDERS_TWITTER][founder_twitter_property];
+            founders_twitter_url.push(url);
+          }
         }
-      }
 
-      var html_founders = '<div class=\"' + DIV_CLASS_PRODUCT_FOUNDERS_NAMES_WRAPPER_MODAL + '\">';
-      var index = 0;
-      for (var founder_property in product[FIREBASE_PRODUCT_FOUNDERS]) {
+        var html_founders = '<div class=\"' + DIV_CLASS_PRODUCT_FOUNDERS_NAMES_WRAPPER_MODAL + '\">';
+        var index = 0;
+        for (var founder_property in product[FIREBASE_PRODUCT_FOUNDERS]) {
+          var founder = product[FIREBASE_PRODUCT_FOUNDERS][founder_property]; // Replace 'product[FIREBASE_PRODUCT_FOUNDERS][founder_property]' with 'founder'
 
-        // Check wether we have twitter url for this founder
-        if (index < founders_twitter_url.length) {
+          html_founders += '<div class=\"row\">';
+          // Check wether we have twitter url for this founder
+          html_founders += '<span class=\"' + DIV_CLASS_TAG + '\" style=\"background-color:' + FOUNDER_TAG_COLOR + '\">' + founder + '</span>';
+          if (index < founders_twitter_url.length) {
             var twitter_url = founders_twitter_url[index];
             var twitter_url_href = '<a target="_blank" href=\"' + twitter_url + '\">' + '<i class=\"fa fa-twitter fa-lg\" aria-hidden=\"true\"></i>' + '</a>'
-            html_founders += product[FIREBASE_PRODUCT_FOUNDERS][founder_property] + ' ' + twitter_url_href + '<br/>';
+            html_founders += ' ' + twitter_url_href + '<br/>';
+            //html_founders += product[FIREBASE_PRODUCT_FOUNDERS][founder_property] + ' ' + twitter_url_href + '<br/>';
+          }
+          else {
+            //html_founders += product[FIREBASE_PRODUCT_FOUNDERS][founder_property] + '<br/>';
+          }
+          html_founders += '</div>';
+          index++;
         }
-        else {
-          html_founders += product[FIREBASE_PRODUCT_FOUNDERS][founder_property] + '<br/>';
-        }
-        index++;
-      }
 
-      html_founders += '</div>'; // Close founders-names-wrapper
-      cardModalHTML += '<div class=\"row\">' +
-                        '<div class=\"' + DIV_CLASS_PRODUCT_FOUNDERS_MODAL + '\">' +
-                          '<b>' + DIV_TEXT_PRODUCT_FOUNDERS_MODAL + '</b>' +
-                            '<div class=\"' + DIV_CLASS_PRODUCT_FOUNDERS_NAMES_WRAPPER_MODAL + '\">' +
+        html_founders += '</div>'; // Close founders-names-wrapper
+        cardModalHTML += '<div class=\"row\">' +
+                          '<div class=\"' + DIV_CLASS_PRODUCT_FOUNDERS_MODAL + '\">' +
+                            '<b>' + DIV_TEXT_PRODUCT_FOUNDERS_MODAL + '</b>' +
                               html_founders +
-                            '</div>' +
-                        '</div>' +
-                      '</div>';
+                          '</div>' +
+                        '</div>';
+      }
 
       // Add platforms //
       if (FIREBASE_PRODUCT_PLATFORMS in product) {
-        var platforms = [];
-        for (var platform_property in product[FIREBASE_PRODUCT_PLATFORMS]) {
-          platforms.push(product[FIREBASE_PRODUCT_PLATFORMS][platform_property]);
-        }
-        platformsPrintable = platforms.join(', ');
         cardModalHTML += '<div class=\"row\">' +
                           '<div class=\"' + DIV_CLASS_PRODUCT_PLATFORMS_MODAL + '\">' +
-                            '<b>' + DIV_TEXT_PRODUCT_PLATFORMS_MODAL + '</b>' + platformsPrintable +
-                          '</div>' +
-                        '</div>';
+                            '<b>' + DIV_TEXT_PRODUCT_PLATFORMS_MODAL + '</b>';
+        var platforms = [];
+        for (var platform_property in product[FIREBASE_PRODUCT_PLATFORMS]) {
+          var platform = product[FIREBASE_PRODUCT_PLATFORMS][platform_property];
+          var platform_color = randomColorFromString(platform, tag_colors);
+          cardModalHTML += '<span class=\"' + DIV_CLASS_TAG + '\" style=\"background-color:goldenrod' /*+ platform_color*/ + '\">' + platform + '</span>';
+        }
+        cardModalHTML += '</div></div>';
       }
 
       // Add API //
@@ -323,8 +356,14 @@ $(document).ready(function() {
                     platform_subtech.push(platform_technology[platform_subtechnology_property]);
                   }
                 }
+                for (var i = 0; i < platform_subtech.length; i++) {
+                  var elem = platform_subtech[i];
+                  var elem_color = randomColorFromString(elem, tag_colors);
+                  html += '<span class=\"' + DIV_CLASS_TAG + '\" style=\"background-color:' + elem_color + '\">' + elem + '</span>';
+                }
                 var platform_subtech_printable = platform_subtech.join(', ');
-                html += platform_subtech_printable + '</div>';
+
+                //html += platform_subtech_printable + '</div>';
               }
             }
             cardModalHTML += html;
@@ -363,38 +402,61 @@ $(document).ready(function() {
       $('div.content').last().append(cardModalHTML);
       /* ----- */
     }
+
+
+
+
+
+
+
     // After cards are load we either want to set the height of cards for same size or let them act dynamically.
     changeHeightOfCards();
-  });
+    /* ----- Search ----- */
+    var products_modal = document.getElementsByClassName('modal'); // We want to search in modals
+    var products_static = document.getElementsByClassName('card'); // But we want to hide statics
+    var search = function() {
+      var query = document.getElementById(SEARCHBOX_ID).value;
+      var match = new Function;
+
+      if (!query.length) {
+        match = function() { return true; }
+      }
+      else if (query.substr(0, query.indexOf(':')) == ':' ) {
+
+      }
+      else {
+        match = function (product) {
+          return (product.textContent.toLowerCase().indexOf(query.toLowerCase()) >= 0);
+        }
+      }
+
+      for (var i = 0; i < products_modal.length; i++) {
+        if (match(products_modal[i]))
+          products_static[i].parentElement.style.display = '';
+        else {
+          products_static[i].parentElement.style.display = 'none';
+        }
+      }
+    }
+    document.getElementById(SEARCHBOX_ID).addEventListener('input', search);
+
+    /* ----- Search using tags in modal ----- */
+    var tags = document.getElementsByClassName(DIV_CLASS_TAG);
+    for (var i = 0; i < tags.length; i++) (function (tag) {
+      tag.addEventListener('click', function (e) {
+        $(this).closest('.modal').modal('toggle');
+        document.getElementById(SEARCHBOX_ID).value = tag.textContent;
+        search();
+      })
+    })(tags[i]);
+    /* ---------- */
+  }); // firebase END
+
 
   // We want to change height of cards according to the size of window (e.g. different for mobile)
   $(window).bind('resize',function() {
     changeHeightOfCards();
-});
+  });
 
-  /* ----- Search ----- */
-  var search = function() {
-    var products = document.getElementsByClassName('card');
-    var query = document.getElementById(SEARCHBOX_ID).value;
-    var match = new Function;
 
-    if (!query.length) {
-      match = function() { return true; }
-    }
-    else {
-      match = function (product) {
-        return (product.textContent.toLowerCase().indexOf(query.toLowerCase()) >= 0);
-      }
-    }
-
-    for (var i = 0; i < products.length; i++) {
-      if (match(products[i]))
-        products[i].parentElement.style.display = '';
-      else {
-        products[i].parentElement.style.display = 'none';
-      }
-    }
-  }
-  document.getElementById(SEARCHBOX_ID).addEventListener('input', search);
-  /* ---------- */
 }); // page loaded END
